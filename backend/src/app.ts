c79c49a31d85e41d74700a38477f2b7b51b9eaec
Akanksha987 <agrawalakanksha23123@gmail.com>
2026-03-11 22:11:39 +0530

    Added swagger ui for the clinican

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { swaggerUi, swaggerSpec } from "./utils/swagger";
+
 import clinicianRoutes from "./routes/clinician.routes";
 import treatmentRoutes from "./routes/treatment.routes";
 
@@ -8,5 +10,9 @@ app.use(express.json());
 
 app.use("/clinicians", clinicianRoutes);
 app.use("/treatment-plans", treatmentRoutes);
+
+/* Swagger UI */
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
