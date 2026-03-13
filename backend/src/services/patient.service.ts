ad755cab1e019335adec44b8f032e99cb4a721ea
Akanksha987 <agrawalakanksha23123@gmail.com>
2026-03-13 17:30:33 +0530

    Added delete endpoint and also updated required changes for creating patient

--- backend/src/services/patient.service.ts
+++ backend/src/services/patient.service.ts
@@ -1,10 +1,20 @@
 import { Patient } from "../models/patient.model";
 import { PatientInput } from "../types/patient.types";
+import { Clinician } from "../models/clinician.model";
 
 export const createPatient = async (data: PatientInput) => {
+
+  const clinician = await Clinician.findById(data.clinicianId);
+
+  if (!clinician) {
+    throw new Error("Clinician ID is not present in the database");
+  }
+
   const patient = new Patient(data);
+
   return await patient.save();
 };
+
 
 export const getPatientById = async (id: string) => {
   return await Patient.findById(id).populate("clinicianId");
@@ -18,9 +28,17 @@ export const updatePatient = async (
   id: string,
   data: Partial<PatientInput>
 ) => {
-  return await Patient.findByIdAndUpdate(id, data, {
-    new: true
-  });
+
+  if (data.clinicianId) {
+
+    const clinician = await Clinician.findById(data.clinicianId);
+
+    if (!clinician) {
+      throw new Error("Clinician ID is not present in the database");
+    }
+  }
+
+  return await Patient.findByIdAndUpdate(id, data, { new: true });
 };
 
 export const deletePatient = async (id: string) => {
